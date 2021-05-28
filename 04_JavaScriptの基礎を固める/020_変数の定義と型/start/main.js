a008f76b0c55b8ebc543c17d626a2c54d763e999
milan2229 <milan_1899.22@icloud.com>
2021-05-28 18:27:34 +0900

    chapter54 finish

---
+++ 04_JavaScriptの基礎を固める/020_変数の定義と型/start/main.js
@@ -0,0 +1,15 @@
+// var, let, const
+
+const name = 'Tom';
+// name ='Tim'
+console.log('hello' + name );
+
+// 動的型付け言語
+let variable = 'str'
+variable = 12;
+variable = false;
+console.log(typeof variable);
+variable = undefined;
+
+// 静的型付け言語
+int num = 12;   
---
+++ 04_JavaScriptの基礎を固める/025_関数定義について/start/main.js
@@ -0,0 +1,9 @@
+const hello = function (name, age) {
+  // let name ='Code Mafia'
+  console.log('hello' + name + age);
+  return name + age;
+}
+
+hello('Code Mafia', 10);
+const returnVal = hello('Code Mafia2', 20);
+console.log(returnVal)
